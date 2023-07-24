f773baf09e78cbd6d60d89a00324f28b3c471160
pori777 <koichionepiece0729@icloud.com>
2023-07-24 18:01:05 +0900

    初期設定

---
+++ archive-works.php
@@ -0,0 +1,42 @@
+<?php get_header() ;?>
+    <article id="mainvisual" class="p-mainvisual">
+        <div class="p-mainvisual__ttlset"></div>
+    </article>
+    <section id="works" class="p-section--post">
+        <h2 class="c-section-ttlset">WORKS</h2>
+        <div>
+            <?php
+            $args = array(
+                'post_type' => 'works',
+                'posts_per_page' => 6,
+                'orderby'=>'post_date', // 投稿日順に並べる
+                'order'=>'DESC' // 新着順に並べる
+            );
+                $the_query = new WP_Query($args); if($the_query->have_posts()):
+            ?>
+            <ul>
+                <?php while ($the_query->have_posts()):$the_query->the_post(); ?>
+                <li>
+                    <a href="<?php the_permalink(); ?>">
+                        <div>
+                            <img src="<?php echo CFS()->get('thumbnail') ;?>" alt="サンプル画像">
+                        </div>
+                        <div>
+                            <time><?php echo get_the_date('Y.m.d')?></time>
+                            <p><?php echo CFS()->get('title') ;?></p>
+                        </div>
+                    </a>
+                </li>
+                <?php endwhile; ?>
+                <?php wp_reset_postdata(); ?>
+            </ul>
+            <?php else : ?>
+            <?php endif ;?>
+            <div class="c-button--post">
+                <a href="<?php echo get_page_link( 113 );?>">
+                    <p>一覧ページを見る<i class="fas fa-angle-right"></i></p>
+                </a>
+            </div>
+        </div>
+    </section>
+<?php get_footer() ;?>
